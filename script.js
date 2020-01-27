af91040dfce42e86f938ddf75e39f011615da0ea
Payton Jewell <paytontaylor96@gmail.com>
2020-01-27 13:35:11 -0600

    basic html, no styling, added click-for-done and double-click-to-remote functions

---
+++ script.js
@@ -0,0 +1,36 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+        li.onclick = function() {
+            li.style.cssText = 'text-decoration: line-through;'
+        }
+        li.ondblclick = function() {
+            this.parentNode.removeChild(this);
+        }
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
