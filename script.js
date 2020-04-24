b691e9c7544a7301b00e5086746a78be96458c18
Payton Jewell <paytontaylor96@gmail.com>
2020-04-24 18:46:45 -0500

    changes and updates

--- script.js
+++ script.js
@@ -1,52 +1,81 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+let today = document.querySelector("#weekday");
+let monthday = document.querySelector("#monthday")
+let month = document.querySelector("#month");
+let hour = document.querySelector("#hour");
+let minutes = document.querySelector("#minutes");
+let ampm = document.querySelector("#ampm");
+setInterval(() => {
+  let now = new Date();
+  let day = days[now.getDay()];
+  let monthname = months[now.getMonth()];
+  let notArmy = now.getHours() - 12;
+  today.innerHTML = `${day}, `;
+  monthday.innerHTML = `${now.getDate()} of `;
+  month.innerHTML = monthname;
+  if (now.getHours() > 12) {
+    hour.innerHTML = notArmy;
+    ampm.innerHTML = "PM"
+  } else {
+    hour.innerHTML = now.getHours();
+    ampm.innerHTML = "AM";
+  }
+  minutes.innerHTML = now.getMinutes() + " ";
+}, 1000)
 
 
-function inputLength() {
-	return input.value.length;
+
+
+
+
+
+
+/* actual to do list stuff */
+
+const input = document.querySelector("input[type='text']");
+const ul = document.querySelector("ul");
+const deleteButton = document.querySelectorAll("ul a");
+
+let numberItems = ul.children.length;
+console.log(numberItems);
+
+const inputLength = () => input.value.length;
+
+const removeItem = (event) => {
+  event.target.parentNode.remove();
+  numberItems--;
+  console.log(numberItems);
 }
-function crossOff(event) {
-	var target = event.target.parentNode;
-	target.classList.toggle("done");
+
+for (let buttons of deleteButton) {
+  buttons.addEventListener("click", removeItem);
 }
 
-function removeParent(event) {
-event.target.parentNode.remove();
-}
-
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-	li.innerHTML = " " + li.innerHTML + " ";
-
-	var check = document.createElement("button");
-	check.innerHTML = "&#10003;";
-	check.onclick = crossOff;
-
-	var button = document.createElement("button");
-	button.innerHTML = 'X';
-	button.onclick = removeParent;
-
-	li.prepend(check);
-	li.appendChild(button);
+const newTask = () => {
+  let v = input.value;
+  let li = document.createElement("li");
+  li.innerHTML = "<label><input type='checkbox'><i></i><span>" + v + "</span><a href='#'>–</a></label>";
+  ul.appendChild(li);
+  input.value = "";
+  let button = document.querySelectorAll("ul a");
+  let removeli = (event) => {
+    event.target.parentNode.remove();
+    numberItems--;
+    console.log(numberItems);
+  }
+  for (let one of button) {
+    one.addEventListener("click", removeli);
+  }
 
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+const addTaskEnter = (e) => {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    newTask();
+    numberItems++;
+    console.log(numberItems);
+  }
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+input.addEventListener("keypress", addTaskEnter);
